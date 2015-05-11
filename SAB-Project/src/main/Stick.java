df260a4379d01219f01f599f9fd2994bc45824ba
Daravenox <jonas.hongisto@pp.inet.fi>
2015-05-11 16:06:34 +0200

    J

--- SAB-Project/src/main/Stick.java
+++ SAB-Project/src/main/Stick.java
@@ -10,6 +10,7 @@ public class Stick extends MouseAdapter{
 	private Square B;
 	private int X;
 	private int Y;
+	private boolean sideways;
 
 	public Stick(Square A, Square B) {
 		this.A = A;
@@ -18,6 +19,8 @@ public class Stick extends MouseAdapter{
 		//System.out.println("Bug finder:\nA = " + A + "\nB = "+B);
 		A.addStick(this);
 		B.addStick(this);
+		sideways = (A.getX()==X);	//Assigns true or false to the sideways field depending on the x coordinate of the A planet.
+
 
 	}
 	public Stick(Square A){
@@ -27,7 +30,7 @@ public class Stick extends MouseAdapter{
 		visible = false;
 		A.addStick(this);
 	}
-//heyhey
+
 	public void place(){
 		if(!chosen){
 			visible = true;
@@ -38,36 +41,42 @@ public class Stick extends MouseAdapter{
 	public boolean isVisible(){
 		return visible;
 	}
-	
+
+	public boolean sideways(){
+		return  sideways;	
+	}
+
 	public Square getA(){
 		return A;
 	}
 	public Square getB(){
 		return B;
 	}
-	
+
 	public void setX(int x){
 		X = x;
 	}
 	public void setY(int y){
 		Y = y;
 	}
-	
+
 	public int getX(){
 		return X;
 	}
 	public int getY(){
 		return Y;
 	}
-	
+
 	@Override
 	public void mouseEntered(MouseEvent e){
-		System.out.println("Entered " + e.getX() + " , " + e.getY() + " for stick: " + this.getX() + ", " + this.getY());
+		//System.out.println("Entered " + e.getX() + " , " + e.getY() + " for stick: " + this.getX() + ", " + this.getY());
 	}
-	
+
 	@Override
 	public void mouseClicked(MouseEvent e){
-		System.out.println("Clicked! ");
+		System.out.println("Stick placed!!");
+		place();
+		System.out.println(visible);
 	}
 }
 
